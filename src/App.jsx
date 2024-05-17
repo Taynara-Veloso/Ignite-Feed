c4642034c961cf52238e16d4c744faaf86b20895
taynara veloso <taynarasantos2.3@hotmail.com>
2024-05-17 03:05:38 -0300

    :star: App sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Header } from "./components/Header";
 
+import styles from './App.module.css'
 import './global.css';
+
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
-      <Header/>     
+      <Header/>    
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>main</main>
+      </div> 
     </div>
   )
 }

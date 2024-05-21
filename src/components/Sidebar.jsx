165673f4491da63577700a945a8e9b46a1a48d96
taynara veloso <taynarasantos2.3@hotmail.com>
2024-05-20 21:44:08 -0300

    :star: Avatar -change and prop hasBorder

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css'
 import { PencilLine } from 'phosphor-react'
 
@@ -9,8 +10,8 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1496483648148-47c686dc86a8?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
       />
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar}
+        <Avatar 
+          hasBorder
           src="https://avatars.githubusercontent.com/u/81330596?v=4" 
         />
         <strong>Taynara Veloso</strong>

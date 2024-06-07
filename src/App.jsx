ade4e756ddbdcf8bb127c91abb3c39b70f65b916
taynara veloso <taynarasantos2.3@hotmail.com>
2024-06-07 20:28:53 -0300

    :star: utilizando array nos posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,70 @@ import { Post } from "./components/Post"
 import styles from './App.module.css'
 import './global.css';
 
+//iteração significa (repetir algo) - criar estrutura de repetição  
+
+const posts = [
+  {
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/81330596?v=4',
+      name: 'Taynara Veloso',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Eai galera!!'},
+      {type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quaerat laboriosam odit harum veniam maxime libero accusamus est doloribus! Repellendus perspiciatis deserunt, quas sunt at placeat autem unde voluptas tempore.'},
+      {type: 'link', content: 'taynarasantos@hotmail.com'},
+    ],
+    publishedAt: new Date('2024-05-23 19:30:00')
+  },
+  {
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/72281058?v=4',
+      name: 'Augusto Cesar',
+      role: 'Escrevente'
+    },
+    content: [
+      {type: 'paragraph', content: 'Olá Pessoal'},
+      {type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe inventore excepturi laboriosam quos illum non unde similique voluptatibus assumenda commodi, asperiores rerum ullam totam, voluptatem quibusdam, porro voluptate atque earum.'},
+      {type: 'link', content: 'augustoCesarSilva19@hotmail.com'},
+    ],
+    publishedAt: new Date('2024-05-23 19:30:00')
+  },
+  {
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/6643122?v=4',
+      name: 'Mayk Brito',
+      role: 'Educador @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala Dev!'},
+      {type: 'paragraph', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat delectus totam aut provident ullam quas commodi odio culpa? Fugit rerum velit officiis aperiam accusantium voluptas facilis, nisi deserunt aliquam magni!'},
+      {type: 'link', content: 'maykBrito1@gmail.com'},
+    ],
+    publishedAt: new Date('2024-05-23 19:30:00')
+  },
+];
+
 export function App() {
   return (
     <div>
       <Header/>    
       <div className={styles.wrapper}>
         <Sidebar/>
-        <Post/>
+        <main>
+          {posts.map(post => {
+            return (
+              <Post key={post}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
       </div> 
     </div>
   )
 }
+
+

f646dcb60679f312d8f7d5decb01a4f568f28fe4
taynara veloso <taynarasantos2.3@hotmail.com>
2024-06-07 20:29:52 -0300

    :star: implementação dos dados do array na pág Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,32 +1,50 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+import { format, formatDistanceToNow} from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import styles from './Post.module.css'
 
-export function Post() {
+const comments = [
+  1,
+  2,
+  3,
+  4,
+];
+
+export function Post({author, publishedAt, content}) {
+  const publishedDateFormatted = format(publishedAt, " d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+  
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  }); 
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar 
-            hasBorder
-            src="https://camo.githubusercontent.com/dfdebe92274e943e9bdecefb4ca5197b9a6a68933ef8eeb8d77665d4dec2619a/68747470733a2f2f692e696d6775722e636f6d2f5470675571544d2e706e67" 
+          <Avatar src={author.avatarUrl} 
           />
           <div className={styles.authorInfo}>
-            <strong>Taynara Veloso</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="17 de Maio às 18:30h" dateTime="2024-05-17 18:30:00">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="">jane.design/doctorcare</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if(line.type === 'paragraph') {
+            return <p key={line}>{line.content}</p>;
+          } else if(line.type === 'link') {
+            return <p key={line}><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
@@ -39,9 +57,10 @@ export function Post() {
         </footer>
       </form>
 
-      <div className='styles.commentList'>
-        <Comment/>
-        <Comment/>
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   );

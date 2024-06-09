489444e7274c154053b3089bc0fa2fce3374d619
taynara veloso <taynarasantos2.3@hotmail.com>
2024-06-09 00:08:39 -0300

    :star: delete comment - componente pai Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -37,6 +37,13 @@ export function Post({author, publishedAt, content}) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment != commentToDelete;
+    })
+    setComments(commentsWithoutDeletedOne);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -80,7 +87,9 @@ export function Post({author, publishedAt, content}) {
           return (
             <Comment 
               key={comment}
-              content={comment}/>
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
           )
         })}
       </div>
